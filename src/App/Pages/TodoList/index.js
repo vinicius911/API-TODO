5b5d80a70d457f7d3f19c780fa4f7525c8a479c1
Vinicius Moslaves <vinicius.moslaves09@gmail.com>
2022-08-28 23:22:04 -0300

    todo list

--- src/App/Pages/TodoList/index.js
+++ src/App/Pages/TodoList/index.js
@@ -1,5 +1,37 @@
+import "./styles.scss";
+import { Button, TextField, Card, Checkbox } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
 export default function TodoList() {
-    return (
-        <div>Usuário</div>
-    );
+  return (
+    <div className="container-todo">
+      <div>
+        <div className="title">Lista de Tarefas</div>
+        <div className="text-field-container">
+          <div className="text-field">
+            <TextField
+              fullWidth
+              size="small"
+              id="outlined-basic"
+              label="Nova tarefa"
+              variant="outlined"
+            />
+          </div>
+          <div className="button">
+            <Button>Adicionar tarefa</Button>
+          </div>
+        </div>
+        <div className="card-container">
+          <Card className="card">
+            <Checkbox defaultChecked />
+            <div>VOTAR NO MITO MITO</div>
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
